a2f800ac32090bb934bae9641ac5bfe328712f91
testkitschen <katryn.mcintosh@gmail.com>
2014-06-17 16:07:05 -0400

    Set and reset the price after changing the discount percentage.

--- lib/track.rb
+++ lib/track.rb
@@ -1,10 +1,21 @@
 class Track
 
-  attr_reader :title, :artists, :duration, :price
+  attr_reader :title, :artists, :duration, :origin_price
+  attr_accessor :price, :discount
 
-  def initialize(title, artists, duration, price)
+  def initialize(title, artists, duration, price, discount: 0)
     @title, @artists, @duration, @price = title, artists, duration, price
 
+    @origin_price = price
+  end
+
+  def set_discount(discount)
+    @discount = discount/100.0
+    @price = origin_price * (1 - @discount)
+  end
+
+  def reset_discount
+    @price = @origin_price
   end
 
 end
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -1,12 +1,36 @@
 require_relative "../lib/track"
 describe Track do
+  let(:track) { Track.new("Happy", "Pharrell Williams", 262, 4) }
   describe 'attributes' do
     it 'has a title, list of artists, duration, price' do
-      track = Track.new("Happy", "Pharrell Williams", 262, 1.00)
       expect(track.title).to eq "Happy"
       expect(track.artists).to eq "Pharrell Williams"
       expect(track.duration).to eq 262
-      expect(track.price).to eq 1.00
+      expect(track.price).to eq 4.00
     end
   end
+
+  describe '#set_discount' do
+    it 'reduces the price by a percentage' do
+      track.set_discount(15)
+
+      expect(track.price).to eq 3.4
+
+      track.set_discount(20)
+
+      expect(track.price).to eq 3.2
+    end
+  end
+
+  describe '#reset_discount' do
+    it 'resets the discount to 0' do
+      track.reset_discount
+
+      expect(track.price).to eq 4
+    end
+  end
+
+
+
+
 end
